e1dc78b31f9b8940ed9fb43abba3112f585d6ae8
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2025-06-14 08:35:57 +0800

    Trying to make responsive

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Homepage from "./Homepage.jsx";
 
-
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Homepage />
-  </StrictMode>
+	<StrictMode>
+		<div className='bg-[#0f0f0f]'>
+			<Homepage />
+		</div>
+	</StrictMode>
 );
