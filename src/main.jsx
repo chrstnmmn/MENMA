abc746b4482fa2e263a1a853d5d3661aaa56188b
Christian Emmanuel <christianemmanuelcollo23@gmail.com>
2025-06-14 10:52:03 +0800

    okay now the title is responsive

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./animation.css";
 import Homepage from "./Homepage.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<div className='bg-[#0f0f0f]'>
-			<Homepage />
-		</div>
-	</StrictMode>
+  <StrictMode>
+    <div className="bg-[#0f0f0f]">
+      <Homepage />
+    </div>
+  </StrictMode>
 );
