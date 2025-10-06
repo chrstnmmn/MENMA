7f76a51bc2fa11d10f2f96eedbc25b2e6fd501fa
chrstnmmnl <christianemmanuelcollo23@gmail.com>
2025-10-07 05:07:13 +0800

    finally fixed the ios issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles/index.scss";
 import "./styles/portfolio-container.scss";
 import "./styles/hero-section.scss";
+import "./styles/introduction-section.scss";
 
 import traceTopRight from "./assets/img/pcb-traces/hero-section/hero-section-pcb-traces-top-right.png";
 import traceBottomLeft from "./assets/img/pcb-traces/hero-section/hero-section-pcb-traces-bottom-left.png";
@@ -8,29 +9,42 @@ import downIcon from "./assets/img/icons/down-icon.png";
 
 function HeroSection() {
 	return (
-		<div className="portfolio-wrapper">
-			{/* Background traces */}
-			<img
-				src={traceTopRight}
-				className="trace trace-top-right"
-				alt="PCB trace top right"
-			/>
-			<img
-				src={traceBottomLeft}
-				className="trace trace-bottom-left"
-				alt="PCB trace bottom left"
-			/>
-			<img src={downIcon} className="down-icon" alt="Down icon" />
-
-			{/* Hero section */}
-			<div className="hero-section">
-				<h3 className="subtitle">Christian Emmanuel Collo</h3>
-				<h1 className="main-title">Menma</h1>
+		<div className="hero-section">
+			<div className="traces">
+				{/* Background traces */}
+				<img
+					src={traceTopRight}
+					className="trace trace-top-right"
+					alt="PCB trace top right"
+				/>
+				<img
+					src={traceBottomLeft}
+					className="trace trace-bottom-left"
+					alt="PCB trace bottom left"
+				/>
 			</div>
+			<div className="down-arrow">
+				<img src={downIcon} className="down-icon" alt="Down icon" />
+			</div>
+			<h3 className="subtitle">Christian Emmanuel Collo</h3>
+			<h1 className="main-title">Menma</h1>
+		</div>
+	);
+}
+
+function IntroSection() {
+	return (
+		<div className="intro-section">
+			<div className="intro-wrapper"></div>
 		</div>
 	);
 }
 
 export default function App() {
-	return <HeroSection></HeroSection>;
+	return (
+		<div className="portfolio-wrapper">
+			<HeroSection></HeroSection>
+			{/* <IntroSection></IntroSection> */}
+		</div>
+	);
 }
